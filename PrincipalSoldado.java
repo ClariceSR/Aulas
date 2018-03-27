a9b753e2976e4ed50a797ad3c59394c61e759a5f
ClariceSR <37124907+ClariceSR@users.noreply.github.com>
2018-03-27 19:54:26 -0300

    Add files via upload

---
+++ PrincipalSoldado.java
@@ -0,0 +1,13 @@
+
+package singleton;
+
+public class PrincipalSoldado {
+    public static void main(String[]args){
+        Soldado soldado = new Soldado();
+        soldado.atirarComBasuca();
+        soldado.atirarComBasuca();
+       //soldado.atirarComMetralhadora();
+        //soldado.atirarComPistola();
+    }
+    
+}
